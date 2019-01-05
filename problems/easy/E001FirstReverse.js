7b7a59f5369c8e7469a4d5f5a2c54c478a4b2506
Ousseni bara <33071371+iamousseni@users.noreply.github.com>
2019-01-05 02:49:01 +0100

    It was perferct but i think this is the better way
    It was perferct but i think this is the better way

--- problems/easy/E001FirstReverse.js
+++ problems/easy/E001FirstReverse.js
@@ -13,23 +13,11 @@ const FirstReverse = (str = '') => {
 	if (typeof str !== 'string') {
 		return 'The input must be a string.';
 	}
-	// set up a variable to hold the length of the string
-	// this will not change, so we will set it as a const
-	const len = str.length;
-
-	// set up a variable to hold the new string, which will begin as an empty string
-	let newstr = '';
-
-	/*
-	iterate through the original string, starting at the end, and build up the new string by
-	adding each previous letter (Note: the 'x +=' operator is simply a shorthand
-	for 'x = x +' )
-	*/
-	for (let i = 0; i < len; i++) {
-		newstr += str[len - (i + 1)];
-	}
-	return newstr;
+	
+	return str.split('').reverse().join('');
 };
+
+
 
 module.exports = {
 	FirstReverse
